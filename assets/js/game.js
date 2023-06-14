7a868ed6694867392ad93eb0420bf940d7819330
defhook <defhook152@gmail.com>
2023-06-13 22:03:57 -0700

    started adding functions and string concatenation

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is a test! JS is running!");
+// this creates a function named 'fight'
+
+// function fight() {
+//     window.alert("The fight has begun!");
+// }
+
+// fight();
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+
+// this will do mat and log 20
+console.log(10+10);
+
+// what is this?
+console.log("Our robot's name is " + playerName);
