35aef2c60bf534575521fd5de4c353a0541121fd
defhook <defhook152@gmail.com>
2023-06-27 06:49:05 -0700

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,7 @@ var fight = function (enemyName) {
     // check enemy's health
     if (enemyHealth <= 0) {
       window.alert(enemyName + " has died!");
-    //   award player money for winning
+      //   award player money for winning
       playerMoney = playerMoney + 20;
       // leave while() loop since enemy is dead
       break;
@@ -116,8 +116,25 @@ var fight = function (enemyName) {
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
-  break;
-}
+    
+  if (playerHealth > 0) {
+    // lets player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+    window.alert("Welcome to Robot Gladiators!" + (i + 1));
+
+    // Pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+
+    // Reset enemyHealth before starting new fight
+    enemyHealth = 50;
+
+    // use debugger to pause script from running and check what's going on at the moment in the code
+    // debbuger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+  }
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
+};
